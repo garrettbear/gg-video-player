095d659fad019889045b43f4730362ad07ec3992
Garrett W <garrett@gumgum.com>
2019-10-18 17:01:02 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="demo">
+      <div className="gg-video-player">Hello</div>
+    </div>
+  );
+}
+
+export default App;
