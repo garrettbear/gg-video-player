3c06e08bc9f8361ab3af6cb78a2927712305a076
Garrett W <garrett@gumgum.com>
2019-10-22 16:10:51 -0700

    player styles

--- scripts.js
+++ scripts.js
@@ -30,7 +30,7 @@ function fullscreen() {
 }
 
 function updateButton() {
-  const icon = this.paused ? "►" : "❚ ❚";
+  const icon = this.paused ? "►" : "❚";
   console.log(icon);
   toggle.textContent = icon;
 }
